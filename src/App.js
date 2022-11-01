4e1b8a6651d1a3ed6925a723eee0fbd6ddbf3a7b
christinarosati <chrisrosati22@gmail.com>
2022-11-01 12:57:56 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Hello from header</header>
+      Hello from app
+    </div>
+  );
+}
