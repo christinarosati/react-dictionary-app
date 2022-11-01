49e3ed1774a404712267bf70fe2c030488e33dbd
christinarosati <chrisrosati22@gmail.com>
2022-11-01 16:00:25 -0400

    Started building search engine basic function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello from header</header>
-      Hello from app
+      <div className="container">
+        <header className="App-header">
+          <h1>📖 Dictionary 🔎</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Christina Rosati</small>
+        </footer>
+      </div>
     </div>
   );
 }
