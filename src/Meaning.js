854a607bac33aee43d1a06b5f9da7f206826b48e
christinarosati <chrisrosati22@gmail.com>
2022-11-02 12:40:02 -0400

    changed dictionary response to display only first definition of each meaning with examples

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,16 +2,26 @@ import React from "react";
 
 export default function Meaning(props) {
   console.log(props.meaning);
-  return (
-    <div className="Meaning">
-      <h4>{props.meaning.partOfSpeech}</h4>
-      {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <p>{definition.definition}</p>
-          </div>
-        );
-      })}
-    </div>
-  );
+  if (props.meaning.definitions[0].example) {
+    return (
+      <div className="Meaning">
+        <h4>{props.meaning.partOfSpeech}</h4>
+        <p>
+          <strong>Definition:</strong> {props.meaning.definitions[0].definition}
+        </p>
+        <p>
+          Example: <em>{props.meaning.definitions[0].example}</em>
+        </p>
+      </div>
+    );
+  } else {
+    return (
+      <div className="Meaning">
+        <h4>{props.meaning.partOfSpeech}</h4>
+        <p>
+          <strong>Definition:</strong> {props.meaning.definitions[0].definition}
+        </p>
+      </div>
+    );
+  }
 }
