4ae8aef6a89fb4373cd06d69a4891ece8f0ca39a
christinarosati <chrisrosati22@gmail.com>
2022-11-08 19:02:55 -0500

    Fixed error messages

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export default function App() {
             <a
               href="https://github.com/christinarosati/react-dictionary-app"
               target="_blank"
-              rel="noopener"
+              rel="noreferrer"
             >
               Open-source code
             </a>{" "}
